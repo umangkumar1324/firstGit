441eb9e09b227e4d9f800baa687d22cc261751a6
Umang Kumar <umangkumar@umangs-air-4.home>
2020-08-25 16:36:13 -0700

    Initial commit

---
+++ SwiftUiTest/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  SwiftUiTest
+//
+//  Created by Umang Kumar on 8/25/20.
+//  Copyright © 2020 Umang Kumar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Spacer()
+
+         HStack{
+               Image("lemon")
+                   Spacer()
+               Image("lemon")
+               Spacer()
+              Image("lemon")
+               Spacer()
+               }
+            Spacer()
+            HStack{
+                  Image("lemon")
+                        Spacer()
+                    Image("lemon")
+                    Spacer()
+                   Image("lemon")
+                    Spacer()
+            }
+            Spacer()
+            HStack{
+                   Image("lemon")
+                        Spacer()
+                    Image("lemon")
+                    Spacer()
+                   Image("lemon")
+                    Spacer()
+                   }
+            Spacer()
+
+        }
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
